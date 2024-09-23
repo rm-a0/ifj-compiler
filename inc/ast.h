d549b0bb63a326811ed92f07365e39292af75cfc
rm-a0 <michalrepcik9@gmail.com>
2024-09-23 23:29:35 +0200

    added todo comments

--- inc/ast.h
+++ inc/ast.h
@@ -15,4 +15,13 @@ typedef struct {
     int todo;
 } ASTNode;
 
+/*
+ * TODO
+ * Create function for allocating and freeing memory for ASTNode
+ * Define grammar for LL
+ * Create parsing function that parses tokens
+ * Optional: Create different types of ASTNodes based on LL grammar
+ * Finish parser that builds the tree
+*/
+
 #endif // AST_H
