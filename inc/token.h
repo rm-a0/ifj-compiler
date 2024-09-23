a4d0627e88ee308ebf1a9da61e8f90c7ba844f81
rm-a0 <michalrepcik9@gmail.com>
2024-09-23 14:03:23 +0200

    added more token types and updated token struct

--- inc/token.h
+++ inc/token.h
@@ -8,17 +8,45 @@
 #define TOKEN_H
 
 /**
+ * @enum TokenType
  * @brief Constants for different token types
- * @param TOKEN_IF
- * @param TOKEN_ELSE
+ * 
+ * @param TOKEN_CONST           Constant for 'const' keyword.
+ * @param TOKEN_ELSE            Constant for 'else' keyword.
+ * @param TOKEN_FN              Constant for 'fn' keyword.
+ * @param TOKEN_IF              Constant for 'if' keyword.
+ * @param TOKEN_I32             Constant for 'i32' keyword.
+ * @param TOKEN_F64             Constant for 'f64' keyword.
+ * @param TOKEN_NULL            Constant for 'null' keyword.
+ * @param TOKEN_PUB             Constant for 'pub' keyword.
+ * @param TOKEN_RETURN          Constant for 'return' keyword.
+ * @param TOKEN_U8              Constant for 'u8' keyword.
+ * @param TOKEN_VAR             Constant for 'var' keyword.
+ * @param TOKEN_VOID            Constant for 'void' keyword.
+ * @param TOKEN_WHILE           Constant for 'while' keyword.
+ * @param TOKEN_IDENTIFIER      Constant for identifier.
  */
 typedef enum {
+    TOKEN_CONST,
+    TOKEN_ELSE,
+    TOKEN_FN,
     TOKEN_IF,
-    TOKEN_ELSE
+    TOKEN_I32,
+    TOKEN_F64,
+    TOKEN_NULL,
+    TOKEN_PUB,
+    TOKEN_RETURN,
+    TOKEN_U8,
+    TOKEN_VAR,
+    TOKEN_VOID,
+    TOKEN_WHILE,
+    TOKEN_IDENTIFIER,
 } TokenType;
 
 /**
+ * @struct Token
  * @brief Struct containing Token info
+ * 
  * @param token_type type of token
  * @param value token value
  */
@@ -28,7 +56,9 @@ typedef struct {
 } Token;
 
 /**
+ * @fn Token get_token()
  * @brief Returns token
+ * 
  * @param[in] None
  * @return Token
 */
