7d0f8872c617c55973830cdedb706e7e71811179
rm-a0 <michalrepcik9@gmail.com>
2024-09-27 23:28:14 +0200

    created files for keyword hashtable

---
+++ inc/keyword_htab.h
@@ -0,0 +1,10 @@
+/**
+ * @file keyword_htab.h
+ * @brief Header file containing implementation of hash table for keyword tokens
+ * @authors Michal Repcik (xrepcim00)
+*/
+
+#ifndef KEYWORD_HTAB_H
+#define KEYWORD_HTAB_H
+
+#endif // KEYWORD_HTAB_H
