28ff6c060d766055887d914a1099c1dabead145f
rm-a0 <michalrepcik9@gmail.com>
2024-09-28 19:02:22 +0200

    added smore states to my fsm

--- src/lexer.c
+++ src/lexer.c
@@ -45,32 +45,63 @@ Token* get_token(Lexer* lexer) {
     while ((c = fgetc(lexer->src)) != EOF) {
         switch (lexer->state) {
             case START:
-                if (isspace(c)) {
+                switch (c) {
+                    case ' ':
+                    case '\t':
+                    case '\n':
+                        break;
+                    case '/':
+                        lexer->state = FWD_SLASH;
+                        break;
+                    case '"':
+                        lexer->state = STRING;
+                        break;
+                    case '0':
+                        lexer->state = ZERO;
+                        break;
+                    case '?':
+                        lexer->state = Q_MARK;
+                        // return ? token
+                        break;
+                    case '_':
+                        lexer->state = UNDERSCORE;
+                        break;
+                    case '[':
+                        lexer->state = L_SQ_BRACKET;
+                        break;  
+                    default:
+                        if (isalpha(c)) {
+                            lexer->state = ID_OR_KEY;
+                        }
+                        else if (c > '0' && c <= '9') {
+                            lexer->state = INTEGER;
+                        }
+                        else if (lexer->ascii_l_table[c] != INVALID) {
+                            // precreate tokens for single characters
+                            return create_token(lexer->ascii_l_table[c], 1, (char)c);
+                        }
+                        else {
+                            // If we encounter unexpected character
+                            exit(LEXICAL_ERROR);
+                        }
+                        break;
+                }
+                break;
+            case ID_OR_KEY:
+                if (isalnum(c) || isalpha(c) || c == '_') {
                     break;
                 }
-                else if (lexer->ascii_l_table[c] != INVALID) {
-                    // precreate tokens for single characters instead of 
-                    // allocating them using malloc
-                    return create_token(lexer->ascii_l_table[c], 1, (char)c);
+                else if (isspace(c)) {
+                    // return either id or keyword
+                    // implement hashtable for keywords
+                    lexer->state = START;
                 }
-                else if (c == '/') {
-                    lexer->state = FWD_SLASH;
+                else {
+                    // invalid identifier
+                    exit(LEXICAL_ERROR);
                 }
-                else if (c == '"') {
-                    lexer->state = STRING;
-                }
-                else if (c == '0') {
-                    lexer->state = ZERO;
-                }
-                else if (c == '?') {
-                    lexer->state = Q_MARK;
-                }
-                else if (c == '_') {
-                    lexer->state = UNDERSCORE;
-                }
-                else if (c == '[') {
-                    lexer->state = L_SQ_BRACKET;
-                }
+                break;
+            case STRING:
                 break;
         }
 
