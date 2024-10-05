e5f467c83be134882c2afa49975aeb6cd70632d5
rm-a0 <michalrepcik9@gmail.com>
2024-10-05 20:02:22 +0200

    added abasic structure for ast and node types

--- inc/ast.h
+++ inc/ast.h
@@ -7,12 +7,37 @@
 #ifndef AST_H
 #define AST_H
 
+typedef enum {
+    AST_PROGRAM,
+    AST_DECL,
+    AST_ASSIGN,
+    AST_FN_DEF,
+    AST_FN_CALL,
+    AST_IF,
+    AST_WHILE,
+    AST_RETURN,
+    AST_BLOCK, // inside {}
+    AST_TYPE_ANOT, // var x: i32 = 0; i32 is typa anot for variable x
+    AST_BIN_OP // = - == != <= ...
+    // add more
+} ASTNodeType;
+
 /**
  * @struct ASTNode
  * @brief TODO
 */
 typedef struct {
-    int todo;
+    ASTNodeType type;
+    union {
+        struct {
+            char* name;
+        } identifier;
+
+        struct {
+            int value;
+        } number_literal;
+        // add more        
+    };
 } ASTNode;
 
 /*
