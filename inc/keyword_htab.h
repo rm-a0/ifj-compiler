058cf9053b28f9d6318c1c7dffad874aad907382
rm-a0 <michalrepcik9@gmail.com>
2024-10-05 19:48:14 +0200

    defined basic function name

--- inc/keyword_htab.h
+++ inc/keyword_htab.h
@@ -7,4 +7,22 @@
 #ifndef KEYWORD_HTAB_H
 #define KEYWORD_HTAB_H
 
+#include "token.h"
+
+typedef struct{
+    int size;
+    KeywordHtabItem* items;
+} KeywordHtab;
+
+typedef struct {
+    char* keyword;
+    TokenType keyword_token;
+} KeywordHtabItem;
+
+void create_keyword_htab();
+
+void init_keyword_htab();
+
+void hash_keyword();
+
 #endif // KEYWORD_HTAB_H
---
+++ src/keyword_htab.c
@@ -0,0 +1,6 @@
+/**
+ * @file keyword_htab.c
+ * @brief File contatining definitions of functions for hash table manipulation
+ * @authors Michal Repcik (xrepcim00)
+*/
+
