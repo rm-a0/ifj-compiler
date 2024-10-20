c7a17c5731b447d3b61bccc5322af6e55e64f076
TheBoBek <simon44bobko@gmail.com>
2024-10-20 12:29:04 +0200

    Updated grammar.go

--- doc/grammar.go
+++ doc/grammar.go
@@ -14,67 +14,100 @@
 
 // Root grammat definitions
 1. <program> 			::= <top_level_decl_list>
-2. <top_level_decl_list>::= <top_level_decl> <top_level_decl_list | ε
-3. <top_level_decl> 	::= <fn_decl> | <const_decl> | <var_decl>
+
+2. <top_level_decl_list>::= <top_level_decl> <top_level_decl_list 
+3. <top_level_decl_list	::= ε
+
+4. <top_level_decl> 	::= <fn_decl>
+5. <top_level_decl		::= <const_decl>
+6. <top_level_decl		::= <var_decl>
 
 // Most used main components
-4. <data_type> 		::= <type_prefix> <type>
-5. <type>			::= "void" | "u8" | "i32" | "f64" | "[]u8"
-6. <type_prefix>	::= "?" | ε
+7. <data_type> 		::= <type_prefix> <type>
+8. <type>			::= "void" | "u8" | "i32" | "f64" | "[]u8"
+9. <type_prefix>	::= "?" 
+10.<type_prefix>	::= ε
 
 // Funcion declaration
-7. <fn_decl> 			::= "pub" "fn" identifier "(" <param_list> ")" <data_type> <block>
-8. <param_list> 		::= <param> <param_list1> | ε
-9. <param_list1> 		::= "," <param> <param_list1> | ε
-10.<param> 	 			::= identifier ":" <data_type>
+11. <fn_decl> 			::= "pub" "fn" identifier "(" <param_list> ")" <data_type> <block>
+12. <param_list> 		::= <param> <param_list1>
+13. <param_list>		::= ε
+14. <param_list1> 		::= "," <param> <param_list1>
+15. <param_list1>		::= ε
+16.	<param> 	 		::= identifier ":" <data_type>
 
 
 // Constant and variable declaration
-11. <const_decl> 		::= "const" identifier <type_assign> "=" <expression> ";"
-12. <var_decl>			::= "var" identifier <type_assign> "=" <expression> ";"
-13. <type_assign>		::= ":" <data_type> | ε
+17. <const_decl> 		::= "const" identifier <type_assign> "=" <expression> ";"
+18. <var_decl>			::= "var" identifier <type_assign> "=" <expression> ";"
+19. <type_assign>		::= ":" <data_type>
+20. <type_assign>		::= ε
 
 // Expression (probably the most crucial part)
-14. <expression>     ::= <term> <expression1>
-15. <expression1>    ::= "+" <term> <expression1> | "-" <term> <expression1> | ε
+21. <expression>     ::= <term> <expression1>
+22. <expression1>    ::= "+" <term> <expression1>
+23.	<expression1>	 ::= "-" <term> <expression1> 
+24. <expression1>	 ::= ε
 
-16. <term>           ::= <factor> <term1>
-17. <term1>          ::= "*" <factor> <term1> | "/" <factor> <term1> | ε
+25. <term>           ::= <factor> <term1>
+26. <term1>          ::= "*" <factor> <term1>
+27. <term1>			 ::= "/" <factor> <term1>
+28. <term1>			 ::= ε
 
-18. <factor>         ::= <number> | identifier <factor_suffix> | string_literal | "(" <expression> ")"
-19. <factor_suffix>	 ::= "(" <arg_list> ")" | ε
+29. <factor>         ::= <number>
+30. <factor>		 ::= identifier <factor_suffix>
+31. <factor>		 ::= string_literal
+32. <factor>		 ::= "(" <expression> ")"
 
-20. <number>         ::= integer | float
+33. <factor_suffix>	 ::= "(" <arg_list> ")"
+34. <factor_suffix>	 ::= ε
+
+35. <number>         ::= integer
+36. <number>		 ::= float
 
 
 // Block
-21. <block>				::= "{" <statement_list> "}"
-22. <statement_list> 	::= <statement> <statement_list1> 
-23. <statement_list1>	::= <statement> <statement_list1> | ε
-24. <statement> 		::=	<const_decl> | <var_decl> | <assignment> | <if_statement> | <while_statement> | <return_statement>
+37. <block>				::= "{" <statement_list> "}"
+38. <statement_list> 	::= <statement> <statement_list1> 
+39. <statement_list1>	::= <statement> <statement_list1>
+40. <statement_list1>	::= ε
+
+41. <statement> 		::=	<const_decl> 
+42. <statement> 		::= <var_decl>
+43. <statement>			::= <assignment>
+44. <statement>			::= <if_statement>
+45. <statement>			::= <while_statement>
+46. <statement>			::= <return_statement>
 
 // Assignment
-25. <assignment> 		::= identifier "=" <assignment1> ";"
-26. <assignment1>		::= <expression> | <built_in_fn>
+47. <assignment> 		::= identifier "=" <assignment1> ";"
+48. <assignment1>		::= <expression> 
+49. <assignment1>		::= <built_in_fn>
 
 
 // Function call (inside expression) and function call statement (inside block)
-27. <arg_list> 			::= <argument> | ε
-28. <argument>			::= <factor> <argument1>
-29. <argument1>			::= "," <argument1> | ε
-30. <built_in_fn> 		::= "ifj" "." <built_in_string> "(" <arg_list> ")"
-31. <built_in_string>	::= "write" | "readstr" | "readi32" | "readf64" | "i2f" | "f2i" | "string" | "length" | "concat" | "substring" | "strcmp" | "ord" | "chr"
+50. <arg_list> 			::= <argument> 
+51. <arg_list>			::= ε
+52. <argument>			::= <factor> <argument1>
+53. <argument1>			::= "," <argument1>
+54. <argument1>			::= ε
+55. <built_in_fn> 		::= "ifj" "." <built_in_string> "(" <arg_list> ")"
+56. <built_in_string>	::= "write" | "readstr" | "readi32" | "readf64" | "i2f" | "f2i" | "string" | "length" | "concat" | "substring" | "strcmp" | "ord" | "chr"
 
 // If and while statements (can only be 1 or 2 expression ?)
-32. <if_statement>		::= "if" "(" <expression> <bin_expression> ")" <element_bind> <block> <else_statement>
-33. <else_statement>	::= "else" <block> | ε
-34. <while_statement> 	::= "while" "(" <expression> <bin_expression> ")" <element_bind> <block>
-35. <bin_expression> 	::= <binary_operator> <expression> | ε
-36. <element_bind> 		::= "|" identifier "|" | ε
+57. <if_statement>		::= "if" "(" <expression> <bin_expression> ")" <element_bind> <block> <else_statement>
+58. <else_statement>	::= "else" <block>
+59. <else_statement>	::= ε
+60. <while_statement> 	::= "while" "(" <expression> <bin_expression> ")" <element_bind> <block>
+61. <bin_expression> 	::= <binary_operator> <expression>
+62. <bin_expression>	::= ε
+63. <element_bind> 		::= "|" identifier "|" 
+64. <element_bind>		::= ε
 
 // Return statement
-37. <return_statement> 	::= "return" <return_statement1> ";"
-38. <return_statement1>	::= <expression> | ε
+65. <return_statement> 	::= "return" <return_statement1> ";"
+66. <return_statement1>	::= <expression>
+67. <return_statement1	::= ε
 
 // Binary operators
-39. <binary_operator> 	::= "==" | "!=" | "<=" | ">=" | ">" | "<"
+68. <binary_operator> 	::= "==" | "!=" | "<=" | ">=" | ">" | "<"
