853132a727e9f197fc07a70528681049f71b53a5
rm-a0 <michalrepcik9@gmail.com>
2024-11-17 23:01:52 +0100

    removed unnecessary const char with tokens

--- src/parser.c
+++ src/parser.c
@@ -9,52 +9,6 @@
 #include "stack.h"
 #include "ast_node_stack.h"
 
-const char* tok_name[] = {
-    "INVALID",              ///< Constant for invalid tokens.
-    "TOKEN_EOF",            ///< Constant for end of file tokens.
-    "TOKEN_CONST",          ///< Constant for 'const' keyword.
-    "TOKEN_ELSE",           ///< Constant for 'else' keyword.
-    "TOKEN_FN",             ///< Constant for 'fn' keyword.
-    "TOKEN_IF",             ///< Constant for 'if' keyword.
-    "TOKEN_I32",            ///< Constant for 'i32' keyword.
-    "TOKEN_F64",            ///< Constant for 'f64' keyword.
-    "TOKEN_NULL",           ///< Constant for 'null' keyword.
-    "TOKEN_PUB",            ///< Constant for 'pub' keyword.
-    "TOKEN_RETURN",         ///< Constant for 'return' keyword.
-    "TOKEN_U8",             ///< Constant for 'u8' keyword.
-    "TOKEN_VAR",            ///< Constant for 'var' keyword.
-    "TOKEN_VOID",           ///< Constant for 'void' keyword.
-    "TOKEN_WHILE",          ///< Constant for 'while' keyword.
-    "TOKEN_IDENTIFIER",     ///< Constant for identifier.
-    "TOKEN_STRING",         ///< Constant for string.
-    "TOKEN_INTEGER",        ///< Constant for integer (whole number).
-    "TOKEN_FLOAT",          ///< Constant for float (decimal number).
-    "TOKEN_SLICE",          ///< Constant for slice '[]'.
-    "TOKEN_L_PAREN",        ///< Constant for '(' character.
-    "TOKEN_R_PAREN",        ///< Constant for ')' character.
-    "TOKEN_L_BRACE",        ///< Constant for '{' character.
-    "TOKEN_R_BRACE",        ///< Constant for '}' character.
-    "TOKEN_DOT",            ///< Constant for '.' character.
-    "TOKEN_COMMA",          ///< Constant for ',' character.
-    "TOKEN_COLON",          ///< Constant for ':' character.
-    "TOKEN_SEMICOLON",      ///< Constant for ';' character.
-    "TOKEN_PIPE",           ///< Constant for '|' character.
-    "TOKEN_PLUS",           ///< Constant for '+' character.
-    "TOKEN_MINUS",          ///< Constant for '-' character.
-    "TOKEN_MULT",           ///< Constant for '*' character.
-    "TOKEN_DIV",            ///< Constant for '/' character.
-    "TOKEN_ASSIGN",         ///< Constant for '=' character.
-    "TOKEN_Q_MARK",         ///< Constant for '?' character.
-    "TOKEN_LESS",           ///< Constant for '<' character.
-    "TOKEN_GREATER",        ///< Constant for '>' character.
-    "TOKEN_EXCM",           ///< Constant for '!' character.
-    "TOKEN_LESS_EQU",       ///< Constant for '<=' operator.
-    "TOKEN_GREATER_EQU",    ///< Constant for '>=' operator.
-    "TOKEN_NOT_EQU",        ///< Constant for '!=' operator.
-    "TOKEN_EQU",            ///< Constant for '==' operator.
-    "TOKEN_IMPORT"          ///< Constant for '@import' directive.
-};
-
 void advance_token(Token** token, Lexer* lexer) {
     if (token && *token) {
         free_token(*token);
