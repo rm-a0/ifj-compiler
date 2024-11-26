59ecf759bd99af76ed9f9a1209ef92de57754232
alegzoo <alexaliascasillas@gmail.com>
2024-11-26 13:34:44 +0100

    adjusted tests and script for testing

--- tests/seman_tests/38.zig
+++ tests/seman_tests/38.zig
@@ -2,7 +2,7 @@
 const ifj = @import("ifj24.zig");
 
 pub fn main() void {
-    var y : i32 = -2147483649;
+    var y : i32 = 2147483648;
     y = y + 1;
     ifj.write(y);
 }
--- tests/seman_tests/39.zig
+++ tests/seman_tests/39.zig
@@ -2,7 +2,7 @@
 const ifj = @import("ifj24.zig");
 
 pub fn main() void {
-    const x : i32 = -2 147 483 648;
+    const x : i32 = -2147483649;
     x = x - 1;
     ifj.write(x);
 }
