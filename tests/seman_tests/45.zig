b21e987efc873052f1b0c3768e325df61a440df9
TheBoBek <simon44bobko@gmail.com>
2024-11-26 14:37:09 +0100

    Added more test with Element bind for SA.

---
+++ tests/seman_tests/45.zig
@@ -0,0 +1,10 @@
+// 'count' in while loop must be of nullable type
+const ifj = @import("ifj24.zig");
+
+pub fn main() void {
+    var count: i32 = 5;
+    while (count) |value| {
+        ifj.write(value);
+        count = count - 1;
+    }
+}
---
+++ tests/seman_tests/47.zig
@@ -0,0 +1,12 @@
+// Error with using value before decl
+const ifj = @import("ifj24.zig");
+
+pub fn main() void {
+    ifj.write(value); // Trying to use 'value' before declaration
+    var num: ?i32 = 5;
+    if (num) |value| {
+        ifj.write(value);
+    } else {
+        ifj.write("Number is null");
+    }
+}
